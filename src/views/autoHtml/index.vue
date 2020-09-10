5ccf5c7e8d026acb763710bbfd10e8afd417cbb7
linxa <linxa@amtxts.com>
2020-09-10 19:05:57 +0800

    更改style的样式代码

--- src/views/autoHtml/index.vue
+++ src/views/autoHtml/index.vue
@@ -11,18 +11,14 @@ import editor from "mavon-editor";
 import StyleEditor from "./components/styleEditor";
 import CvEditor from './components/styleEditor';
 const styleEdit = `
+*{
+  transition:all .5s;
+}
 .main{
-  transition:all .3s ease-in-out;
   background-color: #002b36;
   display: flex;
   align-items: center;
 }
-
-/* 代码高亮 */
-.token.selector{ color: rgb(133,153,0); }
-.token.property{ color: rgb(187,137,0); }
-.token.punctuation{ color: yellow; }
-.token.function{ color: rgb(42,161,152); }
 
 .css-code{
   border: 1px solid yellowgreen;
@@ -33,6 +29,12 @@ const styleEdit = `
   height: 80vh;
   overflow:auto;
 }
+
+/* 代码高亮 */
+.token.selector{ color: rgb(133,153,0); }
+.token.property{ color: rgb(187,137,0); }
+.token.punctuation{ color: yellow; }
+.token.function{ color: rgb(42,161,152); }
 
 .css-code::-webkit-scrollbar {
   display: none;
@@ -50,6 +52,13 @@ const styleEdit = `
   margin: 20px;
 }
 `;
+const cvEdit=`
+# 这是h1
+> 这里注解
+
+* 111
+* 222
+`
 export default {
   name: "autoHtml",
   components: {
@@ -67,7 +76,8 @@ export default {
   },
   methods: {
     async renderEdit() {
-      await this.progressivelyShowStyle(10);
+      await this.progressivelyShowStyle(50);
+      await this.progressivelyShowCV(100)
     },
     progressivelyShowStyle(delay) {
       return new Promise((resolve, reject) => {
@@ -86,6 +96,22 @@ export default {
         showNextCode(0);
       });
     },
+    progressivelyShowCV(delay){
+      return new Promise((resolve, reject) => {
+        let cvCodeLength = cvEdit.length;
+        let showNextCode = async (nextCodeIndex) => {
+          if (nextCodeIndex <= cvCodeLength) {
+            this.cvCode = cvEdit.substring(0, nextCodeIndex + 1);
+            setTimeout(() => {
+              showNextCode(nextCodeIndex + 1);
+            }, delay);
+          } else {
+            resolve();
+          }
+        };
+        showNextCode(0);
+      });
+    }
   },
   computed: {
     codeInStyleTag() {
